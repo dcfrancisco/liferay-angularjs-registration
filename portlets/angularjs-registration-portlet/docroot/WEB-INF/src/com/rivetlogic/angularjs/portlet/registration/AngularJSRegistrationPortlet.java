8141e0668026900cdf7d139adcb13e567fb6da73
katalinamarcos <katalina.marcos@rivetlogic.com>
2014-06-05 16:45:09 -0600

    Adding angularjs-registration-portlet, initial version

---
+++ portlets/angularjs-registration-portlet/docroot/WEB-INF/src/com/rivetlogic/angularjs/portlet/registration/AngularJSRegistrationPortlet.java
@@ -0,0 +1,11 @@
+package com.rivetlogic.angularjs.portlet.registration;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class AngularJSRegistrationPortlet
+ */
+public class AngularJSRegistrationPortlet extends MVCPortlet {
+ 
+
+}
